9164f3fae46134a768d9cb03401b02d89e2b0702
Jason Brownlee <jason.brownlee05@gmail.com>
2010-04-06 17:36:58 +1000

    more comments

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -77,44 +77,58 @@ module ExperimentsHelper
     return am+pm
   end
   
+  def get_display_time_labels
+    return ["12:00am","12:00pm","11:30pm"]
+  end
+  
   #
   # graph of today's temperature (server time today)
   #
   def make_day_temp_graph_img_url(data)
-    temp, time, timelabel = [], [], []
-    data.each_with_index do |rec,i| 
-      temp << rec[0].to_f
-      timelabel << rec[1].split('/')[1]
-      time << i
-    end
-
+    # array of temp data
+    temp = []
+    data.each {|rec| temp << rec[0].to_f}
+    # summary data
     min, max, avg = temp.min, temp.max, temp.sum/temp.size.to_f
     range = max-min
     summary = [min, max, avg]    
-    
+    # scale manually
     temp.each_with_index {|v,i| temp[i] = ((v-min)/range)*100.0}
     summary.each_with_index {|v,i| summary[i] = ((v-min)/range)*100.0}
     
     # block out the remainder of the temps for the day as nulls
     (48-temp.length).times { temp << "_"}
     time_labels = get_full_day_labels
+    display_time_labels = get_display_time_labels
     
     base = "http://chart.apis.google.com/chart?"
+    # graph size
     base << "chs=600x240&"
+    # series colors
     base << "chco=0000FF&"
+    # graph title
     base << "chtt=Melbourne Temperature&"
+    # graph type
     base << "cht=lc&"
+    # visible axes
     base << "chxt=x,y,r&"
+    # axis label styles
     base << "chxs=2,0000DD,9,-1,t,FF0000&"
+    # axis tick mark styles
     base << "chxtc=0,10|1,10|2,-600&"    
+    # axis names
     #base << "chl=Temperature|Time&"
-    base << "chxr=1,#{min},#{max},#{(range)/10.0}&"    
+    # axis ranges
+    base << "chxr=1,#{min},#{max},#{(range)/10.0}&"  
+    # axis labels
     base << "chxl=0:|#{time_labels.join('|')}|2:|min|max|mean|&"
-    
-    #base << "chxl=2:|min|avg|max|&" 
-    base << "chxp=2,#{summary.join(',')}&"
+    # axis label positions (!!!not working for axis 0!!!)
+    base << "chxp=0,#{display_time_labels.join(',')}|2,#{summary.join(',')}&"
+    # range for scaling data
+    #base << "chds=#{min},#{max}&"
+    # data
     base << "chd=t:#{temp.join(',')}"
-        
+
     return base
   end
 
