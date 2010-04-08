2ae6e421389e785ee2a222e4ea2f55bb5aac5e88
Jason Brownlee <jason.brownlee05@gmail.com>
2010-04-08 13:29:12 +1000

    added field validation to sensor

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -2,8 +2,9 @@ class Sensor < ActiveRecord::Base
 
 
   validates_presence_of :name, :message=>"name must be provided"
-  validates_presence_of :temp, :message=>"temp must be provided"
+  validates_length_of :name, :maximum=>256, :message=>"length of name must be >=3 <=256"
   
-  # todo numeric, max length, etc
+  validates_presence_of :temp, :message=>"temp must be provided"
+  validates_numericality_of :temp, :message=>"temp must be numeric"
   
 end
